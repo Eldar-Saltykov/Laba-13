7bf800a3b158efe8e6401c83e2f989cb74f56a2b
Eldar-Saltykov <112901922+Eldar-Saltykov@users.noreply.github.com>
2022-12-23 13:13:20 +0300

    Update ind.c

--- ind.c
+++ ind.c
@@ -1,2 +1,22 @@
 #include<stdio.h>
-  
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+	int n, c = 0, result = 0;
+	char A[40];
+	printf("Put your text\n");
+	fgets(A, 40, stdin);
+	n = strlen(A);
+	for (int i = 0; i < n; i++)
+		if ((A[i] == ',') || (A[i] == ';') || (A[i] == '.')) {
+			result += 1;
+		}
+	if (result > 0) {
+		printf("%d", result);
+	}
+	else {
+		printf("%d", 0);
+	}
+	
+}
